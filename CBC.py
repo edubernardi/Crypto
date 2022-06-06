940fd16d9916ebff069806a054760441a6a2eb5d
edubernardi <77065000+edubernardi@users.noreply.github.com>
2022-06-05 22:56:54 -0300

    Working now

--- CBC.py
+++ CBC.py
@@ -1,30 +1,36 @@
 import random
 import numpy as np
 
-expansion_table = [24, 1, 2, 3,
-                   4, 5, 4, 6,
-                   7, 8, 9, 8,
-                   9, 10, 11, 12,
-                   13, 12, 14, 15,
-                   16, 17, 16, 18,
-                   19, 20, 21, 20,
-                   22, 23, 24, 1]
+expansion_table = [32, 1 , 2 , 3 , 4 , 5 , 4 , 5,
+                 6 , 7 , 8 , 9 , 8 , 9 , 10, 11,
+                 12, 13, 12, 13, 14, 15, 16, 17,
+                 16, 17, 18, 19, 20, 21, 20, 21,
+                 22, 23, 24, 25, 24, 25, 26, 27,
+                 28, 29, 28, 29, 30, 31, 32, 1]
 
-s_box = [[0, 1, 7, 3],
-         [5, 2, 4, 7],
-         [2, 6, 3, 1],
-         [6, 4, 0, 5]]
+s_box = [[11, 14, 7, 3],
+         [5, 2, 12, 8],
+         [10, 6, 13, 1],
+         [9, 4, 0, 15]]
 
-def encrypt(input_file, key):
-    if len(key) > 4:
+s_box_inverted = [[11, 7, 13, 9],
+                  [14, 1, 3, 10],
+                  [5, 6, 2, 4],
+                  [12, 8, 0, 15]]
+
+def encrypt(input_file, key, decrypt):
+    if len(key) >= 4:
         key = key[0: 4]
-    while len(key) < 4:
-        key += chr(random.randint(0, 256))
+    else:
+        return
     file = open(input_file, "rb")
-    output = open(input_file + ".crypto", "wb")
+    extension = ".crypto"
+    if decrypt:
+        extension = ".decrypt"
+    output = open(input_file + extension, "wb")
 
     end_of_file = False
-
+    original_key = key
     while not end_of_file:
         buffer = file.read(6)
         if len(buffer) < 1:
@@ -33,93 +39,57 @@ def encrypt(input_file, key):
             buffer = bytearray(buffer)
             while len(buffer) < 6:
                 buffer.append(0)
-            # permutacoes
+
             buffer_bits = np.unpackbits(buffer)
 
-            buffer_bits1 = expand(buffer_bits[0: 24], 32)
-            buffer_bits2 = expand(buffer_bits[24: 48], 32)
-            buffer_bits3 = np.ndarray(64, int)
             round = 1
-            while round < 2:
-                key = key_schedule(key, round)
+            max_rounds = 5
+            key = original_key
+            while round < max_rounds:
+                if decrypt:
+                    key = reverse_key_schedule(original_key, round, max_rounds)
+                else:
+                    key = key_schedule(key, round)
                 key_bytes = bytearray()
                 for char in key:
                     key_bytes.append(ord(char))
-                key_bits = np.unpackbits(key_bytes)
+
+                #Ceasar Cypher
                 i = 0
-                while i < 64:
-                    if i < 32:
-                        if buffer_bits1[i] == key_bits[i]:
-                            buffer_bits3[i] = 0
-                        else:
-                            buffer_bits3[i] = 1
-                        i += 1
+                while i < 4:
+                    key_bytes[i] = (round + key_bytes[i]) % 256
+                    i += 1
+
+                key_bits = expand(np.unpackbits(key_bytes), 48)
+
+                #Decrypt substitution
+                if decrypt:
+                    buffer_bits = substitution(buffer_bits, decrypt)
+
+                #XOR
+                i = 0
+                while i < 48:
+                    if buffer_bits[i] == key_bits[i]:
+                        buffer_bits[i] = 0
                     else:
-                        if buffer_bits2[i - 32] == key_bits[i - 32]:
-                            buffer_bits3[i] = 0
-                        else:
-                            buffer_bits3[i] = 1
-                        i += 1
-                buffer_bits4 = substitution(buffer_bits3)
+                        buffer_bits[i] = 1
+                    i += 1
 
-
+                #Substitution
+                if not decrypt:
+                    buffer_bits = substitution(buffer_bits, decrypt)
                 round += 1
 
-            print(buffer_bits4)
-            buffer = bytearray(np.packbits(buffer_bits4))
-            for byte in buffer:
-                output.write(bytes([byte]))
+            buffer = bytearray(np.packbits(buffer_bits))
 
-def decrypt(input_file, key):
-    if len(key) > 4:
-        key = key[0: 4]
-    while len(key) < 4:
-        key += chr(random.randint(0, 256))
-    file = open(input_file, "rb")
-    output = open(input_file + ".decrypt", "wb")
+            #remove padding
+            if decrypt:
+                new_buffer = bytearray()
+                for byte in buffer:
+                    if int(byte) != 0:
+                        new_buffer.append(byte)
+                buffer = new_buffer
 
-    end_of_file = False
-
-    while not end_of_file:
-        buffer = file.read(6)
-        if len(buffer) < 1:
-            end_of_file = True
-        else:
-            buffer = bytearray(buffer)
-            while len(buffer) < 6:
-                buffer.append(0)
-            # permutacoes
-            buffer_bits = np.unpackbits(buffer)
-
-            buffer_bits1 = expand(buffer_bits[0: 24], 32)
-            buffer_bits2 = expand(buffer_bits[24: 48], 32)
-            buffer_bits3 = np.ndarray(64, int)
-            round = 1
-            while round < 2:
-                key = key_schedule(key, round)
-                key_bytes = bytearray()
-                for char in key:
-                    key_bytes.append(ord(char))
-                key_bits = np.unpackbits(key_bytes)
-                i = 0
-                while i < 64:
-                    if i < 32:
-                        if buffer_bits1[i] == key_bits[i]:
-                            buffer_bits3[i] = 0
-                        else:
-                            buffer_bits3[i] = 1
-                        i += 1
-                    else:
-                        if buffer_bits2[i - 32] == key_bits[i - 32]:
-                            buffer_bits3[i] = 0
-                        else:
-                            buffer_bits3[i] = 1
-                        i += 1
-                buffer_bits4 = substitution(buffer_bits3)
-                round += 1
-
-            print(buffer_bits4)
-            buffer = bytearray(np.packbits(buffer_bits4))
             for byte in buffer:
                 output.write(bytes([byte]))
 
@@ -137,24 +107,44 @@ def key_schedule(key, round):
     return "".join(new_key)
 
 
+def reverse_key_schedule(key, round, max_rounds):
+    original_rounds = round
+    new_key = list(key)
+    round = 1
+    while round < (max_rounds - (original_rounds - 1)):
+        fibonacci = [0, 1]
+        while len(fibonacci) < round * 4:
+            fibonacci.append(fibonacci[len(fibonacci) - 1] + fibonacci[len(fibonacci) - 2])
+        i = 0
+        while i < 4:
+            new_key[i] = chr((ord(new_key[i]) + fibonacci[4 * (round - 1) + i] + ord(new_key[(i - 1) % 4])) % 256)
+            i += 1
+        round += 1
+    return "".join(new_key)
+
+
 def expand(buffer, size):
-    expanded = np.ndarray(32, int)
+    expanded = np.ndarray(size, int)
     for i in range(0, size):
         expanded[i] = buffer[expansion_table[i] - 1]
     return expanded
 
 
-def substitution(original):
+def substitution(original, inverted):
     i = 0
     result = []
-    while i < 64:
+    while i < 48:
         column = bits_to_decimal(original[i: i + 2])
         line = bits_to_decimal(original[i + 2: i + 4])
-        new_values = decimal_to_3bits(s_box[column][line])
+        if inverted:
+            new_values = decimal_to_bits(s_box_inverted[line][column])
+        else:
+            new_values = decimal_to_bits(s_box[line][column])
         for bit in new_values:
             result.append(bit)
         i += 4
     return result
+
 
 def bits_to_decimal(bits):
     sum = 0
@@ -165,10 +155,11 @@ def bits_to_decimal(bits):
         exponent -= 1
     return sum
 
-def decimal_to_3bits(value):
-    result = [0, 0, 0]
-    i = 2
-    while 0 < value < 8:
+
+def decimal_to_bits(value):
+    result = [0, 0, 0, 0]
+    i = 3
+    while 0 < value < 16:
         if value % 2 == 0:
             result[i] = 0
         else:
--- crypto.py
+++ crypto.py
@@ -2,6 +2,7 @@ import CBC
 import sys
 import numpy as np
 
+
 #file_name = input("Insira o nome do arquivo")
 #chave = input("Insira a chave")
 
@@ -9,8 +10,8 @@ if len(sys.argv) == 3:
     file_name = sys.argv[1]
     key = sys.argv[2]
     if ".crypto" in file_name:
-        CBC.decrypt(file_name, key)
+        CBC.encrypt(file_name, key, True)
     else:
-        CBC.encrypt(file_name, key)
+        CBC.encrypt(file_name, key, False)
 else:
-    print("Usage: crypto.py [input file] [key (4 bytes recommended)]")
+    print("Usage: crypto.py [input file] [key (4 bytes long)]")
